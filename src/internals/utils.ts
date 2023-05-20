8ee88fa00c0da877fffc1314fd876ff3aab1afc7
Michael J. Roberts <michael@observerly.com>
2023-05-20 16:07:09 +0100

    feat: Added getPath() helper utility to internals.
    feat: Added getPath() helper utility to internals.

---
+++ src/internals/utils.ts
@@ -0,0 +1,45 @@
+/*****************************************************************************************************************/
+
+// @author         Michael Roberts <michael@observerly.com>
+// @package        @observerly/trpc-nitro-adapter
+// @license        Copyright © 2021-2023 observerly
+
+/*****************************************************************************************************************/
+
+import { TRPCError } from '@trpc/server'
+
+import { type H3Event } from 'h3'
+
+/*****************************************************************************************************************/
+
+export const getPath = (event: H3Event): string => {
+  const params = event.context.params
+
+  if (!params) {
+    // Throw an error if the trpc paramater is not a string or an array:
+    throw new TRPCError({
+      code: 'INTERNAL_SERVER_ERROR',
+      message:
+        'Please ensure that the trpc paramater is defined in your routes file e.g., ./routes/[trpc].ts',
+      cause: 'Nitro Routing Configuration'
+    })
+  }
+
+  if (typeof params.trpc === 'string') {
+    return params.trpc
+  }
+
+  if (typeof params.trpc === 'string') {
+    return params.trpc
+  }
+
+  // Throw an error if the trpc paramater is not a string or an array:
+  throw new TRPCError({
+    code: 'INTERNAL_SERVER_ERROR',
+    message:
+      'Please ensure that the trpc paramater is defined in your routes file e.g., ./routes/[trpc].ts',
+    cause: 'Nitro Routing Configuration'
+  })
+}
+
+/*****************************************************************************************************************/
